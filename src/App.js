3a44b0edcfcea758a40f98452f6c2b7665568265
namwanza <namwanzaronald4@gmail.com>
2020-05-22 14:56:08 +0300

    add npm install in the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// react
+import React from 'react';
+
+// css
+import './App.css';
+
+
+const AppRouter = () => {
+  return (
+    <div className="container">
+      <h1>Hello World !</h1>
+    </div>
+  );
+}
+
+export default AppRouter;
